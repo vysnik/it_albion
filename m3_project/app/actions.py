29d60e95bb49bd3ee522cff28ae75e38ac7bc9a9
Никита Выскубов <vysnik@gmail.com>
2024-06-18 09:36:41 +0300

    Убрал лишнее из actions.py

--- m3_project/app/actions.py
+++ m3_project/app/actions.py
@@ -6,24 +6,12 @@ from django import forms
 from objectpack.ui import ModelEditWindow
 from .ui import UserAddWindow, UserEditWindow
 
-class PermissionForm(forms.ModelForm):
-    class Meta:
-        model = Permission
-        fields = '__all__'
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = '__all__'
-
 
 # Создание ActionPack для каждой модели
 class ContentTypePack(ObjectPack):
     model = ContentType
     add_to_menu = True
-    menu_title = "ContentType"
-    menu_group = "Security"
-    action_model = "ContentType"
+
     add_window = ModelEditWindow.fabricate(model=model)
     edit_window = ModelEditWindow.fabricate(model=model)
 
@@ -31,9 +19,7 @@ class ContentTypePack(ObjectPack):
 class UserPack(ObjectPack):
     model = User
     add_to_menu = True
-    menu_title = "User"
-    menu_group = "Security"
-    action_model = "User"
+
     # add_window = ModelEditWindow.fabricate(model=model)
     # edit_window = ModelEditWindow.fabricate(model=model)
     edit_window = UserEditWindow
@@ -43,18 +29,14 @@ class UserPack(ObjectPack):
 class GroupPack(ObjectPack):
     model = Group
     add_to_menu = True
-    menu_title = "Group"
-    menu_group = "Security"
-    action_model = "Group"
+
     add_window = ModelEditWindow.fabricate(model=model)
     edit_window = ModelEditWindow.fabricate(model=model)
 
 class PermissionPack(ObjectPack):
     model = Permission
     add_to_menu = True
-    menu_title = "Permissions"
-    menu_group = "Security"
-    action_model = "Permission"
+
     add_window = ModelEditWindow.fabricate(model=model)
     edit_window = ModelEditWindow.fabricate(model=model)
 
