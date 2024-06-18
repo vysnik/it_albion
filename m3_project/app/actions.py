b4d18b0d8361300df608d3e5179288ee3dd6bfed
Nikita Vyskubov <vysnik@gmail.com>
2024-06-18 20:08:57 +0300

    исправил requirements.txt, добавил отображение на рабочий стол

--- m3_project/app/actions.py
+++ m3_project/app/actions.py
@@ -11,6 +11,7 @@ from .ui import UserAddWindow, UserEditWindow
 class ContentTypePack(ObjectPack):
     model = ContentType
     add_to_menu = True
+    add_to_desktop = True
 
     add_window = ModelEditWindow.fabricate(model=model)
     edit_window = ModelEditWindow.fabricate(model=model)
@@ -19,6 +20,7 @@ class ContentTypePack(ObjectPack):
 class UserPack(ObjectPack):
     model = User
     add_to_menu = True
+    add_to_desktop = True
 
     # add_window = ModelEditWindow.fabricate(model=model)
     # edit_window = ModelEditWindow.fabricate(model=model)
@@ -29,6 +31,7 @@ class UserPack(ObjectPack):
 class GroupPack(ObjectPack):
     model = Group
     add_to_menu = True
+    add_to_desktop = True
 
     add_window = ModelEditWindow.fabricate(model=model)
     edit_window = ModelEditWindow.fabricate(model=model)
@@ -36,7 +39,9 @@ class GroupPack(ObjectPack):
 class PermissionPack(ObjectPack):
     model = Permission
     add_to_menu = True
+    add_to_desktop = True
 
     add_window = ModelEditWindow.fabricate(model=model)
     edit_window = ModelEditWindow.fabricate(model=model)
+
 
